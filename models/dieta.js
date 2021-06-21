4bf2693c48f162ce76cc8e5bdbfc1410f899958d
DiegoVilte <viltede@hotmail.com>
2021-06-21 17:44:25 -0300

    error Dieta
    se cambio Shema por Schema

--- models/dieta.js
+++ models/dieta.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dietaModel = require('./dieta');
 const {Schema} = mongoose;
 
-const DietaShema =new Schema({
+const DietaSchema =new Schema({
     dietaPorDias: [{type: Schema.ObjectId , ref: 'DietaEstablecida'}],
     nombreDeDieta: {type: String, require: true},
     objetivoDieta: {type: String, require: true}
