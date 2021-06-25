fa71a7ec9f3e6193b74844f3140c13df96ba71f5
Calderon Emilio Jesus <calderonemiliojesus@gmail.com>
2021-06-25 17:39:06 -0300

    Añadiendo seguridad  Cambios a ser confirmados: 	modificado:     routes/usuario.route.js

--- routes/usuario.route.js
+++ routes/usuario.route.js
@@ -7,8 +7,8 @@ const express = require('express');
 const router = express.Router();
 
 //definimos las rutas para la gestion de agente
-router.get('/', usuarioCtrl.getAllUsuario);
-router.post('/', usuarioCtrl.createUsuario);
+router.get('/',autCtrl.verifyToken, usuarioCtrl.getAllUsuario);
+router.post('/',autCtrl.verifyToken, usuarioCtrl.createUsuario);
 
 router.get('/:id',autCtrl.verifyToken, usuarioCtrl.getUsuario);
 router.put('/:id',autCtrl.verifyToken, usuarioCtrl.editUsuario);
